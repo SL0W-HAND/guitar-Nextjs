7e60f21f48a5b716bc634a6a4b80fd3861715252
SL0W-HAND <daniel.carrete.guzman@gmail.com>
2020-12-10 20:01:43 -0600

    add data

---
+++ database/data.js
@@ -0,0 +1,94 @@
+const data = {
+    'd87c27e':{
+        name:'Gibson Les Paul',
+        id:'d87c27e',
+        price:2679.21,
+        image:'/images/guitars/Gibson_Les_Paul.jpg',
+        logo:'/images/brands/Gibson_logo.webp',
+        stars:4.9,
+        description:"Classic designs never die, and the Les Paul only grew in popularity over the years. The 1960s version was adopted by scores of guitarists who went on to become legends in their own right. This Les Paul Standard '60s features a solid mahogany body with an AA figured maple top, and a slim taper '60s-style mahogany neck with a rosewood fingerboard and trapezoid inlays. Loaded with a calibrated pair of Alnico V BurstBucker 61 pickups and hand-wired controls, it's got a huge voice that's always ready to rock."
+    },
+    'ad52459':{
+        name:'Gibson SG',
+        id:'ad52459',
+        price:1607.10,
+        image:'/images/guitars/Gibson_Sg.jpg',
+        description:"The Gibson SG Standard boasts classic looks and a full set of features associated with the desirable late-'60s style SG models. With a rounded-profile mahogany neck; bound, 22-fret rosewood fingerboard; stable, long-tenon, 19th-fret neck joint and a solid mahogany body it delivers that iconic rock tone with singing sustain. The 490R and 490T Alnico II pickups provide the power to drive your amp to new heights, and a black, 5-ply, full-face pickguard put this versatile classic in a category all its own.",
+        logo:'/images/brands/Gibson_logo.webp',
+        stars:4.4
+    },
+    '82099b6':{
+        name:'Gibson Explorer',
+        id:'82099b6',
+        price:1821.52,
+        image:'/images/guitars/Gibson_Explorer.jpg',
+        description:"The second of two radical designs Gibson presented in 1958 the Explorer has remained an object of yearning by players who want classic sounds and a distinctive look. The quest can end here, as this version stands with the best of them. Boasting an all mahogany body and neck, rosewood fingerboard and loaded with tone-packed BurstBucker pickups, it delivers a full range of tone and power. It's a design that will always look modern, and always turn heads. Includes hardshell case.",
+        logo:'/images/brands/Gibson_logo.webp',
+        stars:4.7
+    },
+    'e954ee9':{
+        name:'Gibson Flying V',
+        id:'e954ee9',
+        price:1821.47,
+        image:'/images/guitars/Gibson_Flaying_V.jpg',
+        description:"From its introduction in 1958 to the present, the Gibson Flying V has remained an icon of style, in demand by players who want classic sounds and a distinctive look. This version flies with the best of them. Boasting an all mahogany body and neck, rosewood fingerboard and loaded with tone-packed BurstBucker pickups, it delivers a full range of tone and power. It's a design that will always look modern, and always turn heads. Includes hard case.",
+        logo:'/images/brands/Gibson_logo.webp',
+        stars:4.4
+    },
+    ' 59e6869':{
+        name:'Alvarez Artist Series ABT60 Baritone Guitar Natural',
+        id:' 59e6869',
+        price:428.79,
+        image:'/images/guitars/ALVAREZ_acoustic.jpg',
+        description:"Alvarez has been making the YB1 Alvarez-Yairi Baritone for years and its popular has remained strong throughout its run. Now they've added the Artist Series ABT60 Baritone Guitar, a modern baritone that will surely blow you away. The ABT60 is a member of the Alvarez Artist '60 Series. Alvarez was careful to get the construction just right to allow this series to sing, and deliver a player experience that's exceptional for a mid-price instrument.",
+        logo:'/images/brands/ALVAREZ_Logo.webp',
+        stars:4.5
+    },
+    'eb2af29':{
+        name:'AMERICAN ULTRA STRATOCASTER',
+        id:'eb2af29',
+        price:1001.03,
+        image:'/images/guitars/Fender_American_ultra_Stratocaster.jpg',
+        description:'The American Ultra series of guitars and basses is the most advanced for demanding musicians who demand the ultimate in precision, performance and tone. The American Ultra Stratocaster features a unique "modern D" neck profile with rounded fingerboard edges for hours of comfort, and the neck heel shape allows easy access to the higher register. A fast 10 "compound radius fingerboard at 14 "with 22 medium jumbo frets for precise and effortless solos, while Ultra Noiseless ™ pickups and advanced wiring options provide endless sound possibilities: no hum. This versatile next-generation instrument will inspire you to take your performance to new heights. Other features include sealed locking tuners, chrome hardware and bone nut. Includes premium molded hardshell case.',
+        logo:'/images/brands/Fender_logo.png',
+        stars:4.7
+    },
+    'f51bfa3':{
+        name:'PLAYER TELECASTER® LEFT-HANDED',
+        id:'f51bfa3',
+        price:699.99,
+        image:'/images/guitars/Fender_American_Telecaster.jpg',
+        description:'Bold, innovative and rugged, the Player Telecaster LH is pure Fender, through and through. The feel, the style and, most importantly, the sound—they’re all there, waiting for you to make them whisper or wail for your music. Versatile enough to handle almost anything you can create and durable enough to survive any gig, this workhorse is a trusty sidekick for your musical vision.',
+        logo:'/images/brands/Fender_logo.png',
+        stars:4.5
+    },
+    'c30915a':{
+        name:"VINTERA® '60S JAGUAR®",
+        id:'c30915a',
+        price:1049.99,
+        image:'/images/guitars/Fender_Jaguar.jpg',
+        description:"For players who want the style and sound of Fender’s golden era, we created the Vintera ‘60s Jaguar. Equipped with the coveted features that defined the decade—including period-accurate neck profile and playing feel, along with recreated vintage-style pickups—this guitar has all of the “round”, yet bold, tone that made the Jaguar a legend. For authentic, vintage-style tone we recreated the pair of single-coil Jaguar pickups to sound and feel more like the originals—keeping the focusing cages, of course. The chiming neck pickup is tempered by the bright, aggressive bridge pickup. The “Mid 60s C”-shaped neck has a 7.25”-radius fingerboard with 22 vintage-style frets for classic playing feel. Vintage-style Jaguar bridge, tremolo and tuning machines provide original-era aesthetics, rock-solid performance and tuning stability. Other features include the classic lead/rhythm circuit, chrome hardware and 4-bolt neck plate. Includes a deluxe gig bag.",
+        logo:'/images/brands/Fender_logo.png',
+        stars:4.2
+    },
+    ' afe3230':{
+        name:"AMERICAN ACOUSTASONIC® TELECASTER® FLAG PRINT",
+        id:' afe3230',
+        price:2099.99,
+        image:'/images/guitars/Fender_acoustasonic_Telecaster.jpg',
+        description:"The American Acoustasonic® Telecaster® embodies the spirit of purposeful innovation that Fender was built on. From acoustic shape-shifting to electric rhythm tones, this powerful guitar uses a revolutionary Fender and Fishman®-designed Acoustic Engine to deliver new sonic expression from the studio to the stage.",
+        logo:'/images/brands/Fender_logo.png',
+        stars:4
+    },
+    '042c500':{
+        name:"MONTECITO TENOR UKULELE",
+        id:'042c500',
+        price:279.99,
+        image:'/images/guitars/Fender_Montecito_tenor_ukulele.jpg',
+        description:"Inspired by the California home of many celebrities, this ukulele's screen-ready looks and headline grabbing features could only have one name—the Montecito Ukulele. Constructed completely from koa, this tenor-sized uke has a rich, sonorous tone, that matches its sumptuously elegant aesthetic. The solid top provides enhanced resonance and sustain, along with incredible warmth and well-balanced projection. The Montecito's koa body is complemented by blue-green abalone top binding and a bound back and fingerboard for a refined look. Other features include a no-tie bridge for quick, easy string changes, a bone nut and saddle for superior intonation, an abalone rosette, vintage-style closed-back tuning machines and a 4-in-line Tele® headstock for that unmistakably classic Fender look.",
+        logo:'/images/brands/Fender_logo.png',
+        stars:3.5
+    }
+}
+
+export default data
