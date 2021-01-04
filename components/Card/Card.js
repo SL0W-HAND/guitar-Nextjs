6b8e3b8a6699467b854f2325420a08baf8e622fb
SL0W-HAND <daniel.carrete.guzman@gmail.com>
2021-01-03 20:54:08 -0600

    adding bootstrap

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -7,24 +7,28 @@ export class Card extends Component {
         this.state = {
             price: null,
             image: null,
-            name:""
+            name:"",
+            stars:0
         }
     }
     //<Image className='image' src={this.state.image} alt={this.state.name}/>
 
     componentDidMount(){
-        this.setState({price: this.props.price, image: this.props.image.toString(), name:this.props.name})
+        this.setState({price: this.props.price, image: this.props.image.toString(), name:this.props.name, stars:this.props.stars})
     }
 
     render() {
         return (
             <div className='card'>
-                <h1 className='title'>{this.state.name}</h1>
-                <figure>
-                    <img src={this.state.image} alt={this.state.name.concat(' IMAGE')}/>
-                </figure>
-                <h3>Price: {this.state.price} $</h3>
-                
+                <img src={this.state.image} alt={this.state.name.concat(' IMAGE')} className='card-img-top'/>
+                <div className='card-body'>
+                    <h4 className='card-title'>{this.state.name}</h4>
+                    <h5 className='card-text'>Price: {this.state.price} $</h5>
+                    <div>
+                        <span><p>{this.state.stars}</p></span>
+                        <button className='btn btn-primary'>Add</button>
+                    </div>
+                </div>
             </div>
         )
     }
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,19 +4,21 @@ import Image from 'next/image'
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-           <div className='nav_links'>
-                <Link href="/">
-                    <a >Home</a>
-                </Link>
-                <Link href="/about">
-                    <a >About</a>
-                </Link>   
-            </div>
-            <figure>
-                <Image src='/images/assets/guitar-pick.svg'  width="40" height="40"  alt='favicon'/>
-            </figure>
-        </nav>
+        <div className='nav-container'>
+            <nav className='nav'>
+            <div className='nav_links'>
+                    <Link href="/">
+                        <a >Home</a>
+                    </Link>
+                    <Link href="/about">
+                        <a >About</a>
+                    </Link>   
+                </div>
+                <figure>
+                    <Image src='/images/assets/guitar-pick.svg'  width="40" height="40"  alt='favicon'/>
+                </figure>
+            </nav>
+        </div>
     )
 }
 
